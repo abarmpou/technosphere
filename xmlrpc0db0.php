82e622dbc157f33fd8088a9a97942c2f3f51099b
Angelos Barmpoutis <angelbar@ufl.edu>
2024-01-13 18:18:57 -0500

    First push

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://research.dwi.ufl.edu/projects/technosphere</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://research.dwi.ufl.edu/projects/technosphere/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/technosphere/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/technosphere/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/technosphere/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/technosphere/index.php/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
